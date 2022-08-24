da7808420ee48dafc011d648240765752338bba2
YAHOR <yahorhaichuk@yandex.by>
2022-08-24 20:55:48 +0200

    изменил поле rating делаю миграции

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -24,8 +24,7 @@ class Title(models.Model):
     author = models.ForeignKey(
         CustomUser, on_delete=models.CASCADE, related_name='titles')
     year = models.IntegerField(default=2000)
-    rating = models.ManyToManyField(Review, on_delete=models.SET_NULL,
-                                    related_name='film_rating')
+    rating = models.ManyToManyField(Review, related_name='film_rating')
     description = models.TextField(default='')
     category = models.ForeignKey(
         Category, on_delete=models.SET_NULL, related_name='categories',
