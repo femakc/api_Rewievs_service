c8bad3e7142003254bb3a7410b8c28c3a7552faa
YAHOR <yahorhaichuk@yandex.by>
2022-08-24 11:53:50 +0200

    написаны модели Category, Genre, Title

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# models : rewiew, comments, titles, genres, catigories
+from api_yamdb.users.models import CustomUser
+
+
+class Category(models.Model):
+    name = models.CharField('Категория', max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+
+
+class Genre(models.Model):
+    name = models.CharField('Жанр', max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+
+
+class Title(models.Model):
+    name = models.CharField('Название', max_length=200)
+    author = models.ForeignKey(
+        CustomUser, on_delete=models.CASCADE, related_name='titles')
+    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
+    category = models.OneToOneField(
+        Category, on_delete=models.PROTECT,
+        related_name="posts", blank=True, null=True
+    )
+    genre = models.ForeignKey(
+        Genre, on_delete=models.SET_NULL,
+        related_name="posts", blank=True, null=True
+    )
