2b5845a0e86e486f0a6f1988a37483ebb3c718ad
femakc <97850166+femakc@users.noreply.github.com>
2022-09-02 19:16:01 +0300

    message()

--- api_yamdb/api/send_email.py
+++ api_yamdb/api/send_email.py
@@ -1,10 +1,12 @@
 from django.core.mail import send_mail
 from users.models import User
 
+from api_yamdb.settings import DEFAULT_FROM_EMAIL
 
-def send_mesege(username):
+
+def send_message(username):
     user = User.objects.get(username=username)
     email = user.email
     subject = 'confirmation_code'
     message = f'{username} ваш конфирмайшен код {user.confirmation_code}'
-    return send_mail(subject, message, 'admin@admin.ru', [email])
+    return send_mail(subject, message, DEFAULT_FROM_EMAIL, [email])
